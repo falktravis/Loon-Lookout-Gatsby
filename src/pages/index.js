3faad5c00715b02471148e9a2ec166cf974209ca
skateeveryday10 <falk.travis@gmail.com>
2023-01-24 21:07:57 -0500

    V1.1.3

--- src/pages/index.js
+++ src/pages/index.js
@@ -284,7 +284,7 @@ export const Head = () => {
           <link rel="canonical" href="https://loonlookout.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Plus+Jakarta+Sans&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
       </>
   );
 }
